1e8741c21b3f6fae4d01049bd54843a4c1038eeb
bobokrut <egor-evlampiev@mail.ru>
2022-05-07 18:39:11 +0300

    change: now images are stored on disk and not in the database

--- gallery/models.py
+++ gallery/models.py
@@ -7,12 +7,12 @@ class GalleryImage(db.Model):
     __tablename__ = 'images'
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer(), db.ForeignKey("users.id"))
-    img_filename = db.Column(db.String())
-    img_width = db.Column(db.Integer())
-    img_height = db.Column(db.Integer())
-    img_data = db.Column(db.LargeBinary)
-    img_thumb = db.Column(db.LargeBinary)
+    img_name=db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    img_width = db.Column(db.Integer)
+    img_height = db.Column(db.Integer)
+    img_path = db.Column(db.String, unique=True)
+    thumb_path = db.Column(db.String, unique=True)
 
     def __repr__(self):
         return '<image id={},name={}>'.format(self.id, self.name)
--- gallery/views.py
+++ gallery/views.py
@@ -1,64 +1,104 @@
-from flask import request, redirect, abort, Blueprint, render_template, Response
+from os import path
+from flask import (
+    request,
+    redirect,
+    abort,
+    Blueprint,
+    render_template,
+    Response,
+    send_from_directory,
+)
 from flask.helpers import url_for
 from flask_login import login_required, current_user
+
+from env_var import IMAGES_FOLDER, THUMBS_FOLDER
 from .models import GalleryImage
 from PIL import Image
-from io import BytesIO
 from extensions import db
+import uuid
+from psycopg2.errors import UniqueViolation
+from traceback import print_exception
 
 gallery = Blueprint("gallery", __name__)
 
 
-@gallery.route('/images/<int:img_id>', methods=['GET'])
+@gallery.route("/images/<int:img_id>", methods=["GET"])
 @login_required
 def get_image_from_db(img_id):
-    image = GalleryImage.query.filter(GalleryImage.id == img_id, GalleryImage.user_id == current_user.id).with_entities(GalleryImage.img_data).first()
+    image = (
+        GalleryImage.query.filter(
+            GalleryImage.id == img_id, GalleryImage.user_id == current_user.id
+        )
+        .with_entities(GalleryImage.img_path)
+        .first()
+    )
     if image:
-        return Response(image[0], mimetype='application/octet-stream')
+        return send_from_directory(IMAGES_FOLDER, image[0])
     abort(404)
 
 
-@gallery.route('/images/<int:img_id>', methods=["POST"])
+@gallery.route("/images/<int:img_id>", methods=["POST"])
 @login_required
 def delete_image_from_db(img_id):
 
-    GalleryImage.query.filter(GalleryImage.id==img_id, GalleryImage.user_id == current_user.id).delete()
+    GalleryImage.query.filter(
+        GalleryImage.id == img_id, GalleryImage.user_id == current_user.id
+    ).delete()
     db.session.commit()
     return redirect(url_for("gallery.view_gallery"))
 
 
-@gallery.route('/thumbs/<int:img_id>', methods=['GET'])
+@gallery.route("/thumbs/<int:img_id>", methods=["GET"])
 @login_required
 def get_thumb_from_db(img_id):
-    image = GalleryImage.query.filter(GalleryImage.id == img_id, GalleryImage.user_id == current_user.id).with_entities(GalleryImage.img_thumb).first()
-    if image:
-        return Response(image[0], mimetype='application/octet-stream')
 
+    image = (
+        GalleryImage.query.filter(
+            GalleryImage.id == img_id, GalleryImage.user_id == current_user.id
+        )
+        .with_entities(GalleryImage.thumb_path)
+        .first()
+    )
+    if image:
+            return send_from_directory(THUMBS_FOLDER, image[0])
     abort(404)
 
 
-@gallery.route('/upload', methods=["POST"])
+@gallery.route("/upload", methods=["POST"])
 @login_required
 def upload():
+    def create_uuid(ext: str) -> tuple[str, str]:
+        return f"{str(uuid.uuid4())}.{ext}", f"{str(uuid.uuid4())}.{ext}"
+
     for file in request.files.getlist("photo"):
 
         filename = file.filename
         ext = filename.split(".")[1]
         ext = ext if ext != "jpg" else "jpeg"
-        blob = file.read()
-
-        # start of the thumbnail creation
         image = Image.open(file)
         height = image.height
         width = image.width
         image.thumbnail(size=(250, 250))
-        stream = BytesIO()
-        image.save(stream, ext)
-        # end
-        image = GalleryImage(img_filename=filename, img_data=blob, img_thumb=stream.getvalue(), img_width=width, img_height=height, user_id=current_user.id)
-        db.session.add(image)
 
-    db.session.commit()
+        while True:
+            image_path, thumb_path = create_uuid(ext)
+            image_to_save = GalleryImage(
+                img_name=filename,
+                img_path=image_path,
+                thumb_path=thumb_path,
+                img_width=width,
+                img_height=height,
+                user_id=current_user.id,
+            )
+            try:
+                db.session.add(image_to_save)
+                db.session.commit()
+                break
+            except UniqueViolation:
+                pass
+        image.save(path.join(THUMBS_FOLDER, thumb_path))
+        file.seek(0)
+        file.save(path.join(IMAGES_FOLDER, image_path))
 
     return redirect("/")
 
@@ -66,5 +106,5 @@ def upload():
 @gallery.route("/")
 @login_required
 def view_gallery():
-    images = GalleryImage.query.filter(GalleryImage.user_id == current_user.id).with_entities(GalleryImage.id, GalleryImage.img_width, GalleryImage.img_height).all()
+    images = GalleryImage.query.filter(GalleryImage.user_id == current_user.id).all()
     return render_template("index.html", images=images, username=current_user.username)
