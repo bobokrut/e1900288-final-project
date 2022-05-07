7eaf3c850964468c49f9e7f3413e5eff2ddfc3b8
bobokrut <egor-evlampiev@mail.ru>
2022-05-07 18:16:36 +0300

    add: path for images

--- env_var.py
+++ env_var.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
-from os import environ
+from os import environ, path
 
 load_dotenv()
 DATABASE_URL = environ.get("DATABASE_URL")  # in Heroku this variable is always presenting
 SECRET_KEY = environ.get("SECRET_KEY")
+IMAGES_FOLDER = path.join(path.dirname(__file__), "static", "gallery", "images")
+THUMBS_FOLDER = path.join(path.dirname(__file__), "static", "gallery", "thumbs")  
