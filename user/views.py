8bbc0fcfc475462f8f17602b5eddca0a17ee7e9b
bobokrut <egor-evlampiev@mail.ru>
2022-05-11 23:13:02 +0300

    change: better type hints and formatting

--- user/views.py
+++ user/views.py
@@ -4,6 +4,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from werkzeug.wrappers.response import Response
 from extensions import login_manager
 
+from typing import Any
 
 from extensions import db
 from .models import User
@@ -12,31 +13,34 @@ from .models import User
 user = Blueprint("user", __name__)
 
 
-@user.route("/signup", methods=["POST"])  # type: ignore
+@user.route("/signup", methods=["POST"])
 def signup_post() -> Response:
-    email: str = request.form.get("email")
-    username: str = request.form.get("username")
-    password: str = request.form.get("password")
+    email: str = request.form.get("email")  # type: ignore
+    username: str = request.form.get("username")  # type: ignore
+    password: str = request.form.get("password")  # type: ignore
     user = User.query.filter_by(username=username).first()
 
     if user:  # checkin if user exists in the table
         flash("Username already exists")
         return redirect(url_for("user.login_get"))
 
-    new_user = User(email=email, username=username, password=generate_password_hash(password, method="sha256"))  # type: ignore
-
+    new_user = User(
+        email=email,
+        username=username,
+        password=generate_password_hash(password, method="sha256"),
+    )
     db.session.add(new_user)
     db.session.commit()
 
     return redirect(url_for("user.login_get"))
 
 
-@user.route("/signup", methods=["GET"])  # type: ignore
+@user.route("/signup", methods=["GET"])
 def signup_get() -> str:
     return render_template("signup.html")
 
 
-@user.route("/login", methods=["POST"])  # type: ignore
+@user.route("/login", methods=["POST"])
 def login_post() -> Response:
     username: str = request.form.get("username")  # type: ignore
     password: str = request.form.get("password")  # type: ignore
@@ -52,18 +56,18 @@ def login_post() -> Response:
     return redirect(url_for("gallery.view_gallery"))
 
 
-@user.route("/login", methods=["GET"])  # type: ignore
+@user.route("/login", methods=["GET"])
 def login_get() -> str:
     return render_template("login.html")
 
 
-@user.route("/logout")  # type: ignore
-@login_required  # type: ignore
+@user.route("/logout")
+@login_required
 def logout() -> Response:
     logout_user()
     return redirect(url_for("user.login_get"))
 
 
-@login_manager.user_loader  # type: ignore
+@login_manager.user_loader
 def load_user(user_id: str) -> Any:
     return User.query.get(int(user_id))
