ed1b318dd349b8fca0f7da4588ff27fe8d175c18
bobokrut <egor-evlampiev@mail.ru>
2022-05-23 16:25:53 +0300

    fix: better loading of optional lib

--- env_var.py
+++ env_var.py
@@ -1,8 +1,12 @@
 from os import environ, path
 
-from dotenv import load_dotenv
+try:
 
-load_dotenv()
+    from dotenv import load_dotenv
+    load_dotenv()
+
+except ImportError:
+    pass
 
 if not environ.get("DATABASE_URL"):
     raise NameError("DATABASE_URL is not defined")
@@ -10,7 +14,7 @@ if not environ.get("DATABASE_URL"):
 if not environ.get("SECRET_KEY"):
     raise NameError("SECRET_KEY is not defined")
 
-DATABASE_URL: str = environ.get("DATABASE_URL")  # type: ignore  NOTE: in Heroku this variable is always presenting
+DATABASE_URL: str = environ.get("DATABASE_URL")  # type: ignore #  NOTE: in Heroku this variable is always presenting
 SECRET_KEY: str = environ.get("SECRET_KEY")  # type: ignore
 IMAGES_FOLDER: str = path.join(path.dirname(__file__), "static", "gallery", "images")
 THUMBS_FOLDER: str = path.join(path.dirname(__file__), "static", "gallery", "thumbs")
